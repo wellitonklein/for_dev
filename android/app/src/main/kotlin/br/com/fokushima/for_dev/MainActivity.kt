a335f7bfc73e940d6254c0ce61b6301cafb6ad11
Welliton Klein <welliton.fokushima@gmail.com>
2020-09-16 20:50:26 -0300

    add Authentication Use Case

---
+++ android/app/src/main/kotlin/br/com/fokushima/for_dev/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.fokushima.for_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/entities/account_entity.dart
@@ -0,0 +1,9 @@
+import 'package:meta/meta.dart';
+
+class AccountEntity {
+  final String token;
+
+  AccountEntity({
+    @required this.token,
+  });
+}
