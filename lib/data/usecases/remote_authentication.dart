727de929dfafd4db8dc20af1f3759f5b6d6239cc
Welliton Klein <welliton.fokushima@gmail.com>
2020-09-17 13:52:05 -0300

    make RemoteAuthentication implements IAuthentication

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -6,7 +6,7 @@ import '../../domain/usecases/usecases.dart';
 import '../http/http.dart';
 import '../models/models.dart';
 
-class RemoteAuthentication {
+class RemoteAuthentication implements IAuthentication {
   final IHttpClient httpClient;
   final String url;
 
