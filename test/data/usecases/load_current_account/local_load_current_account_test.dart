c933d56273327e2e0010c9c5acf833c519b870ac
Welliton Klein <welliton.fokushima@gmail.com>
2020-09-20 21:20:45 -0300

    ensure LocalLoadCurrentAccount returns an account on success

--- test/data/usecases/load_current_account/local_load_current_account_test.dart
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -1,9 +1,12 @@
+import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
+import 'package:for_dev/domain/entities/entities.dart';
+
 abstract class IFecthSecureCacheStorage {
-  Future<void> fetchSecure({@required String key});
+  Future<String> fetchSecure({@required String key});
 }
 
 class FetchSecureCacheStorageSpy extends Mock
@@ -14,20 +17,29 @@ class LocalLoadCurrentAccount {
 
   LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
 
-  Future<void> load() async {
-    await fetchSecureCacheStorage.fetchSecure(key: 'token');
+  Future<AccountEntity> load() async {
+    final token = await fetchSecureCacheStorage.fetchSecure(key: 'token');
+    return AccountEntity(token: token);
   }
 }
 
 void main() {
   LocalLoadCurrentAccount sut;
   FetchSecureCacheStorageSpy fetchSecureCacheStorage;
+  String token;
+
+  void mockFetchSecure() {
+    when(fetchSecureCacheStorage.fetchSecure(key: anyNamed('key')))
+        .thenAnswer((_) async => token);
+  }
 
   setUp(() {
     fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
     sut = LocalLoadCurrentAccount(
       fetchSecureCacheStorage: fetchSecureCacheStorage,
     );
+    token = faker.guid.guid();
+    mockFetchSecure();
   });
 
   test('should call FetchSecureCacheStorage with correct value', () async {
@@ -38,5 +50,15 @@ void main() {
 
     // assert
     verify(fetchSecureCacheStorage.fetchSecure(key: 'token'));
+  });
+
+  test('should return an AccountEntity', () async {
+    // arrange
+
+    // act
+    final account = await sut.load();
+
+    // assert
+    expect(account, AccountEntity(token: token));
   });
 }
