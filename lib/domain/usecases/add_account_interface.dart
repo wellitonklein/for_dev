5a8d85481c09d765338487d35a081018f3973146
Welliton Klein <welliton.fokushima@gmail.com>
2020-09-22 23:00:11 -0300

    add AddCount Use Case

---
+++ lib/domain/usecases/add_account_interface.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../entities/entities.dart';
+
+abstract class IAddAccount {
+  Future<AccountEntity> add({@required AddAccountParams params});
+}
+
+class AddAccountParams extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+  AddAccountParams({
+    @required this.name,
+    @required this.email,
+    @required this.password,
+    @required this.passwordConfirmation,
+  });
+
+  @override
+  List<Object> get props => [
+        name,
+        email,
+        password,
+        passwordConfirmation,
+      ];
+}
--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,3 +1,4 @@
+export './add_account_interface.dart';
 export './authentication_interface.dart';
 export './load_current_account_interface.dart';
 export './save_current_account_interface.dart';
