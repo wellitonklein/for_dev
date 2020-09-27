13116d390821a98acaa4c755ca16b53a85fabf6e
Welliton Klein <welliton.fokushima@gmail.com>
2020-09-27 20:55:01 -0300

    ensure SignUpPage show loading before signUp

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -18,6 +18,7 @@ void main() {
   StreamController<UIError> passwordErrorController;
   StreamController<UIError> passwordConfirmationErrorController;
   StreamController<bool> isFormValidController;
+  StreamController<bool> isLoadingController;
 
   void initStreams() {
     nameErrorController = StreamController<UIError>();
@@ -25,6 +26,7 @@ void main() {
     passwordErrorController = StreamController<UIError>();
     passwordConfirmationErrorController = StreamController<UIError>();
     isFormValidController = StreamController<bool>();
+    isLoadingController = StreamController<bool>();
   }
 
   void mockStreams() {
@@ -38,6 +40,8 @@ void main() {
         .thenAnswer((_) => passwordConfirmationErrorController.stream);
     when(presenter.isFormValidStream)
         .thenAnswer((_) => isFormValidController.stream);
+    when(presenter.isLoadingStream)
+        .thenAnswer((_) => isLoadingController.stream);
   }
 
   void closeStreams() {
@@ -46,6 +50,7 @@ void main() {
     passwordErrorController.close();
     passwordConfirmationErrorController.close();
     isFormValidController.close();
+    isLoadingController.close();
   }
 
   Future<void> loadPage(WidgetTester tester) async {
@@ -255,20 +260,44 @@ void main() {
     expect(button.onPressed, isNull);
   });
 
+  testWidgets('should call authentication on form submit',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    final button = find.byType(RaisedButton);
+
+    isFormValidController.add(true);
+    await tester.pump();
+    await tester.ensureVisible(button);
+    await tester.tap(button);
+    await tester.pump();
+
+    verify(presenter.signUp()).called(1);
+  });
+
   testWidgets(
-    'should call authentication on form submit',
+    'should present loading',
     (WidgetTester tester) async {
       await loadPage(tester);
 
-      final button = find.byType(RaisedButton);
-
-      isFormValidController.add(true);
-      await tester.pump();
-      await tester.ensureVisible(button);
-      await tester.tap(button);
+      isLoadingController.add(true);
       await tester.pump();
 
-      verify(presenter.signUp()).called(1);
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'should hide loading',
+    (WidgetTester tester) async {
+      await loadPage(tester);
+
+      isLoadingController.add(true);
+      await tester.pump();
+      isLoadingController.add(false);
+      await tester.pump();
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     },
   );
 }
