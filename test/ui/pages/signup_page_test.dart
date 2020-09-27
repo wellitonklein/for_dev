86b8f40fe11ad47ce84d69ab23fa364c3b6fc894
Welliton Klein <welliton.fokushima@gmail.com>
2020-09-27 20:16:52 -0300

    ensure SignUpPage present email error

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -138,4 +138,26 @@ void main() {
     );
     verify(presenter.validatePasswordConfirmation(password));
   });
+
+  testWidgets('should present email error', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    emailErrorController.add(UIError.invalidField);
+    await tester.pump();
+    expect(find.text('Campo inválido.'), findsOneWidget);
+
+    emailErrorController.add(UIError.requiredField);
+    await tester.pump();
+    expect(find.text('Campo obrigatório.'), findsOneWidget);
+
+    emailErrorController.add(null);
+    await tester.pump();
+    expect(
+      find.descendant(
+        of: find.bySemanticsLabel('Email'),
+        matching: find.byType(Text),
+      ),
+      findsOneWidget,
+    );
+  });
 }
