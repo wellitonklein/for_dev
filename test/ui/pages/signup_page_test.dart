7630b90ab916b349163e58fe4dc5008afb2e7ca5
Welliton Klein <welliton.fokushima@gmail.com>
2020-10-05 19:26:34 -0300

    remove unused tests

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -86,62 +86,6 @@ void main() {
     closeStreams();
   });
 
-  testWidgets('should load with correct initial state',
-      (WidgetTester tester) async {
-    // arrange
-    await loadPage(tester);
-
-    // assert
-    final nameTextChildren = find.descendant(
-      of: find.bySemanticsLabel('Nome'),
-      matching: find.byType(Text),
-    );
-    expect(
-      nameTextChildren,
-      findsOneWidget,
-      reason:
-          'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
-    );
-
-    final emailTextChildren = find.descendant(
-      of: find.bySemanticsLabel('Email'),
-      matching: find.byType(Text),
-    );
-    expect(
-      emailTextChildren,
-      findsOneWidget,
-      reason:
-          'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
-    );
-
-    final passwordTextChildren = find.descendant(
-      of: find.bySemanticsLabel('Senha'),
-      matching: find.byType(Text),
-    );
-    expect(
-      passwordTextChildren,
-      findsOneWidget,
-      reason:
-          'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
-    );
-
-    final passwordConfirmationTextChildren = find.descendant(
-      of: find.bySemanticsLabel('Confirmar senha'),
-      matching: find.byType(Text),
-    );
-    expect(
-      passwordConfirmationTextChildren,
-      findsOneWidget,
-      reason:
-          'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
-    );
-
-    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
-    expect(button.onPressed, null);
-
-    expect(find.byType(CircularProgressIndicator), findsNothing);
-  });
-
   testWidgets('should call validate with correct values',
       (WidgetTester tester) async {
     await loadPage(tester);
