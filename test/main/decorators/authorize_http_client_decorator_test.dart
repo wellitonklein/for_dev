9db23e524e7469a4c5e145b265d852fdf3975669
Welliton Klein <welliton.fokushima@gmail.com>
2020-10-06 12:00:11 -0300

    move AuthorizeHttpClientDecorator to its own file

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -1,43 +1,10 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
 import 'package:for_dev/data/cache/cache.dart';
 import 'package:for_dev/data/http/http.dart';
-
-class AuthorizeHttpClientDecorator implements IHttpClient {
-  final IFetchSecureCacheStorage fetchSecureCacheStorage;
-  final IHttpClient decoratee;
-
-  AuthorizeHttpClientDecorator({
-    @required this.fetchSecureCacheStorage,
-    @required this.decoratee,
-  });
-
-  Future<dynamic> request({
-    @required String url,
-    @required String method,
-    Map body,
-    Map headers,
-  }) async {
-    try {
-      final token = await fetchSecureCacheStorage.fetchSecure(key: 'token');
-      final authorizedHeaders = headers ?? {}
-        ..addAll({'x-access-token': token});
-      return await decoratee.request(
-        url: url,
-        method: method,
-        body: body,
-        headers: authorizedHeaders,
-      );
-    } on HttpError {
-      rethrow;
-    } catch (_) {
-      throw HttpError.forbidden;
-    }
-  }
-}
+import 'package:for_dev/main/decorators/decorators.dart';
 
 class FetchSecureCacheStorageSpy extends Mock
     implements IFetchSecureCacheStorage {}
