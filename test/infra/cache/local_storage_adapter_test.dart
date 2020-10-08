912d09218b7aeaa1447d5b91025b1e04870fe6b3
Welliton Klein <welliton.fokushima@gmail.com>
2020-10-08 11:16:43 -0300

    move LocalStorageAdapter to its own file

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -2,18 +2,8 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:localstorage/localstorage.dart';
-import 'package:meta/meta.dart';
 
-class LocalStorageAdapter {
-  final LocalStorage localStorage;
-
-  LocalStorageAdapter({@required this.localStorage});
-
-  Future<void> save({@required String key, @required dynamic value}) async {
-    await localStorage.deleteItem(key);
-    await localStorage.setItem(key, value);
-  }
-}
+import 'package:for_dev/infra/cache/cache.dart';
 
 class LocalStorageSpy extends Mock implements LocalStorage {}
 
@@ -36,29 +26,31 @@ void main() {
     value = faker.randomGenerator.string(50, min: 50);
   });
 
-  test('should call localStorage with correct values', () async {
-    // act
-    await sut.save(key: key, value: value);
-    // assert
-    verify(localStorage.deleteItem(key)).called(1);
-    verify(localStorage.setItem(key, value)).called(1);
-  });
+  group('save', () {
+    test('should call localStorage with correct values', () async {
+      // act
+      await sut.save(key: key, value: value);
+      // assert
+      verify(localStorage.deleteItem(key)).called(1);
+      verify(localStorage.setItem(key, value)).called(1);
+    });
 
-  test('should throw if deleteItem throws', () async {
-    // arrange
-    mockDeleteItemError();
-    // act
-    final future = sut.save(key: key, value: value);
-    // assert
-    expect(future, throwsA(TypeMatcher<Exception>()));
-  });
+    test('should throw if deleteItem throws', () async {
+      // arrange
+      mockDeleteItemError();
+      // act
+      final future = sut.save(key: key, value: value);
+      // assert
+      expect(future, throwsA(TypeMatcher<Exception>()));
+    });
 
-  test('should throw if deleteItem throws', () async {
-    // arrange
-    mockSetItemError();
-    // act
-    final future = sut.save(key: key, value: value);
-    // assert
-    expect(future, throwsA(TypeMatcher<Exception>()));
+    test('should throw if deleteItem throws', () async {
+      // arrange
+      mockSetItemError();
+      // act
+      final future = sut.save(key: key, value: value);
+      // assert
+      expect(future, throwsA(TypeMatcher<Exception>()));
+    });
   });
 }
