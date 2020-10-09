5d7afed1d15348967844166adb2c51f49cd8cc9f
Welliton Klein <welliton.fokushima@gmail.com>
2020-10-09 09:42:26 -0300

    move RemoteLoadSurveysWithLocalFallback to ows file

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -1,36 +1,11 @@
 import 'package:faker/faker.dart';
-import 'package:for_dev/domain/helpers/domain_error.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
-import 'package:for_dev/data/usecases/load_surveys/load_surveys.dart';
+import 'package:for_dev/data/usecases/usecases.dart';
 import 'package:for_dev/domain/entities/entities.dart';
-import 'package:for_dev/domain/usecases/usecases.dart';
-
-class RemoteLoadSurveysWithLocalFallback implements ILoadSurveys {
-  final RemoteLoadSurveys remote;
-  final LocalLoadSurveys local;
-
-  RemoteLoadSurveysWithLocalFallback({
-    @required this.remote,
-    @required this.local,
-  });
-
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final surveys = await remote.load();
-      await local.save(surveys);
-      return surveys;
-    } catch (error) {
-      if (error == DomainError.accessDenied) {
-        rethrow;
-      }
-      await local.validate();
-      return await local.load();
-    }
-  }
-}
+import 'package:for_dev/domain/helpers/helpers.dart';
+import 'package:for_dev/main/composites/composites.dart';
 
 class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
 
