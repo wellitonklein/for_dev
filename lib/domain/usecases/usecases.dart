fb8279b0daaf2fb196acf2f797cef29bfec31236
Welliton Klein <welliton.fokushima@gmail.com>
2020-10-09 11:05:34 -0300

    add ILoadSurveyResult use case

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,5 +1,6 @@
 export './add_account_interface.dart';
 export './authentication_interface.dart';
 export './load_current_account_interface.dart';
+export './load_survey_result_interface.dart';
 export './load_surveys_interface.dart';
 export './save_current_account_interface.dart';
