ddfd4c63c14a32f21c545efac98133dd291bd9bd
Welliton Klein <welliton.fokushima@gmail.com>
2020-10-12 17:29:19 -0300

    move SurveyResultWidget to its own file

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../widgets/widgets.dart';
 import 'survey_result_presenter_interface.dart';
+import 'widgets/widgets.dart';
 
 class SurveyResultPage extends StatelessWidget {
   final ISurveyResultPresenter presenter;
@@ -35,72 +36,7 @@ class SurveyResultPage extends StatelessWidget {
               }
 
               if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: 4,
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return Container(
-                        padding: const EdgeInsets.only(
-                          top: 40,
-                          bottom: 20,
-                          left: 20,
-                          right: 20,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).disabledColor.withAlpha(90),
-                        ),
-                        child: Text('Qual é o seu framework web favorito?'),
-                      );
-                    }
-                    return Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).backgroundColor,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Image.network(
-                                'https://pbs.twimg.com/profile_images/875996174305472512/upM71pVR_400x400.jpg',
-                                width: 40,
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10),
-                                  child: Text(
-                                    'Vue.js',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                '100%',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColorDark,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10),
-                                child: Icon(
-                                  Icons.check_circle,
-                                  color: Theme.of(context).highlightColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Divider(height: 1),
-                      ],
-                    );
-                  },
-                );
+                return SurveyResultWidget();
               }
 
               return SizedBox(height: 0);
