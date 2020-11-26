7ce5c93c84260f51aa5d348e6b31a82a69a3ce23
Welliton Klein <welliton.fokushima@gmail.com>
2020-11-26 13:23:13 -0300

    add ISaveSurveyResult use case

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -4,3 +4,4 @@ export './load_current_account_interface.dart';
 export './load_survey_result_interface.dart';
 export './load_surveys_interface.dart';
 export './save_current_account_interface.dart';
+export './save_survey_result_interface.dart';
